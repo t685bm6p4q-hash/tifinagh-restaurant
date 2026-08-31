5b1c1c555144ec0b6d30dfadddf1a5183d0556e7
hamed diabira <hamed.diabira@gmail.com>
2026-09-01 01:51:14 +0200

    Perf: supprimer preloads hero en double

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
 import { HomeTemplate } from '@/src/components/templates/home-template'
 
 export default function Home() {
-  return (
-    <>
-      <link
-        rel="preload"
-        as="image"
-        href="/images/hero-salle-640.webp"
-        fetchPriority="high"
-        media="(max-width: 768px)"
-      />
-      <link
-        rel="preload"
-        as="image"
-        href="/images/hero-salle.webp"
-        fetchPriority="high"
-        media="(min-width: 769px)"
-      />
-      <HomeTemplate />
-    </>
-  )
+  return <HomeTemplate />
 }
